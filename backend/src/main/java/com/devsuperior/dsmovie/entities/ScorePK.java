25b900845ca78c4006905ffc7ffe9dc401ca99e0
Patricia Josianne <patriciajosianne@gmail.com>
2022-01-19 12:55:40 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
@@ -6,6 +6,8 @@ import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+
+
 @Embeddable
 public class ScorePK implements Serializable {
 	private static final long serialVersionUID = 1L;
